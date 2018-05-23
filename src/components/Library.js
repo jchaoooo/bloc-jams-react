c2a13b386f36fb65d2ed0785b37459d97ac536fd
Jessica Chao <JChao@Jessicas-MBP.attlocal.net>
2018-05-22 20:44:14 -0700

    Checkpoint 5 Library: Completed

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import albumData from './../data/albums';
 
-const Library = () => (
-  <section className="library">
-    Library will go here
-  </section>
-);
+class Library extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {albums: albumData }
+  }
+  render() {
+    return(
+      <section className="Library">
+      {
+        this.state.albums.map( (album, index) =>
+          <div key={ index } >
+          <Link to={`/album/${album.slug}`} key={index}>
+            <img src={album.albumCover} />
+            <div>{album.title}</div>
+            <div>{album.artist}</div>
+            <div>{album.songs.length} songs</div>
+          </Link>
+          </div>
+        )
+      }
+      </section>
+    );
+  }
+}
 
 export default Library;
