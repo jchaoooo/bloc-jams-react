c6257bfb04f9187924543afa364816f5df97e4dd
Jessica Chao <jchao@SJ-D5510-H84BQF.brg.com>
2018-05-28 13:47:41 -0700

    Checkpoint 8 PlayerBar Buttons: In progress

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
-
+import PlayerBar from './PlayerBar';
 
 class Album extends Component {
   constructor(props) {
@@ -97,6 +97,11 @@ class Album extends Component {
             )}
           </tbody>
         </table>
+        <PlayerBar
+          isPlaying={this.state.isPlaying}
+          currentSong={this.state.currentSong}
+          handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+         />
       </section>
     );
   }
